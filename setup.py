5796ecb8471101f618c8d3c7e15ca2309b8bbf4f
Brett Jurman <i.be.brett@gmail.com>
2014-10-18 19:13:46 -0400

    added default setup so we can install

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='arachnid',
+    version='0.1',
+    description='arachnid information gathering system',
+    author='Brett Jurman',
+    author_email='i.be.brett@gmail.com',
+    packages=['experiments']
+)
