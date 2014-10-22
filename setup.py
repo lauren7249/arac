1e938a9d2bd5a658f3be1a15332f70809b32eedd
Brett Jurman <i.be.brett@gmail.com>
2014-10-22 06:25:37 +0000

    modifed setup to reflect not using sqlalchemy

--- setup.py
+++ setup.py
@@ -10,12 +10,8 @@ setup(
 
     install_requires = [
         'boto>=2.33.0',
-        'sqlalchemy>=0.9.8',
         'beautifulsoup4>=4.3.2',
         'redis>=2.10.3',
         'rq>=0.4.6'
-    ],
-    dependency_links = [
-        'git+git://git@github.com:ibebrett/python-kinesis-consumer.git'
     ]
 )
