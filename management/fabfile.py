62c242e26547f7c0c9f05d8d5e7ad53ef5f2fcb8
James Johnson <jamesjohnson11@gmail.com>
2014-10-24 13:34:43 -0400

    added new conf and management

--- management/fabfile.py
+++ management/fabfile.py
@@ -1,5 +1,7 @@
 from boto import ec2
-from fabric.api import env, run, sudo
+from fabric import colors
+from fabric.api import *
+from fabric.contrib.project import *
 
 env.user = 'ubuntu'
 env.parallel = True
@@ -12,7 +14,8 @@ def get_all():
     conn = ec2.connect_to_region('us-east-1')
 
     instances = conn.get_only_instances()
-    env.hosts = [instance.ip_address for instance in instances]
+    env.hosts = [instance.ip_address for instance in instances if\
+            instance.ip_address]
 
 def get_bad():
     tailed = run("tail -n 50 /var/log/supervisor/scraper-0")
@@ -25,8 +28,15 @@ def get_bad():
 
 
 def update():
-    get_all()
-    run("cd /home/ubuntu/arachnid")
-    run("git pull origin feature/onlyredis")
-    run("sudo service supervisor restart")
+    with cd("/home/ubuntu/arachnid"):
+        run("git pull origin feature/onlyredis")
+        sudo("service supervisor restart")
     print "Done", env.host
+
+def update_supervisor():
+    with cd("/home/ubuntu/arachnid"):
+        run("git pull origin feature/onlyredis")
+    with cd("etc/supervisor/conf.d/"):
+        sudo("cp /home/ubuntu/arachnid/conf/scraper.conf /etc/supervisor/conf.d/scraper.conf")
+        sudo("service supervisor restart")
+
