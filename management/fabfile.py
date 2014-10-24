32ca261a34e9ecd7687c7a8a5434944242bb25b8
Brett Jurman <i.be.brett@gmail.com>
2014-10-24 10:52:27 -0400

    adding fab file

---
+++ management/fabfile.py
@@ -0,0 +1,25 @@
+from boto import ec2
+from fabric.api import env, run, sudo
+
+env.user = 'ubuntu'
+env.parallel = True
+env.hosts = []
+
+bad_hosts = []
+good_hosts = []
+
+def get_all():
+    conn = ec2.connect_to_region('us-east-1')
+
+    instances = conn.get_only_instances()
+    env.hosts = [instance.ip_address for instance in instances]
+
+def get_bad():
+    tailed = run("tail -n 50 /var/log/supervisor/scraper-0")
+
+    if "raise Phantom" in tailed:
+        print 'Bad host detected', env.host
+
+    else:
+        print 'Host is ok', env.host
+
