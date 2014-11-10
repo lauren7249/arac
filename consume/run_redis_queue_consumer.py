338f6c8a119ec7a4907ea3c36fd3b9b6a4bbeca8
James Johnson <jamesjohnson11@gmail.com>
2014-11-10 17:52:18 -0500

    added ability to upgrade to queue

--- consume/run_redis_queue_consumer.py
+++ consume/run_redis_queue_consumer.py
@@ -9,7 +9,7 @@ from time import sleep
 
 from redis_queue import RedisQueue, get_redis
 
-from consume import process_from_file
+from consume import process_from_file, upgrade_from_file
 
 logger = logging.getLogger('consumer')
 logger.addHandler(logging.StreamHandler())
@@ -25,6 +25,10 @@ def consume_q(q, args):
     real_args = json.loads(args)
     process_from_file(**real_args)
 
+def consume_upgrade_q(q, args):
+    real_args = json.loads(args)
+    upgrade_from_file(**real_args)
+
 def run_q():
     q = get_q()
 
@@ -34,6 +38,22 @@ def run_q():
 
         try:
             consume_q(q, args)
+        except Exception:
+            logger.exception('Exception while processing {}'.format(args))
+            q.fail(args)
+        else:
+            logger.debug('Successfully processed {}'.format(args))
+            q.succeed(args)
+
+def run_upgrade_q():
+    q = get_q()
+
+    # grab the next piece of work
+    while True:
+        args = q.pop_block()
+
+        try:
+            consume_upgrade_q(q, args)
         except Exception:
             logger.exception('Exception while processing {}'.format(args))
             q.fail(args)
@@ -79,6 +99,7 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--queue-range', action='store_true')
     parser.add_argument('--retry-all', action='store_true')
+    parser.add_argument('--upgrade-all', action='store_true')
     parser.add_argument('--url-file')
     parser.add_argument('--start', type=int)
     parser.add_argument('--end', type=int)
@@ -90,6 +111,8 @@ def main():
         queue_range(args.url_file, args.start, args.end, args.range)
     elif args.retry_all:
         retry_all()
+    elif args.upgrade_all:
+        run_upgrade_q()
     else:
         run_q()
 
