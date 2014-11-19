ea868a8a05587a0bbce0be7aea6093011738a3e6
James Johnson <jamesjohnson11@gmail.com>
2014-11-19 16:02:41 -0500

    added jobs to site

--- consume/app.py
+++ consume/app.py
@@ -29,7 +29,7 @@ inner join prospect on prospect.id=prospect_school_user;\
 """
 
 @app.route("/")
-def search():
+def search_schools():
     school_results = None
     if request.args.get("url"):
         s3_key = url_to_key(request.args.get("url"))
@@ -50,8 +50,47 @@ def search():
                 result['current_location'] = prospect[4]
                 result['industry'] = prospect[5]
                 school_results.append(result)
-
     return render_template('home.html', school_results=school_results)
+
+JOB_SQL = """select prospect.name, company_raw, start_date, end_date, \
+job_location, propsect.location_raw, propsect.industry_raw \
+from (select * from (\
+select id as job_id, start_date, end_date, job.user as job_user, company_raw,location as job_location \
+from job where company_raw='%s') as JOBS
+where to_char(start_date, 'YYYY') between '%s' and '%s' OR \
+to_char(end_date, 'YYYY') between '%s' and '%s') AS YEARS \
+INNER JOIN prospect on prospect.id=job_user;\
+"""
+
+@app.route("/jobs")
+def search_jobs():
+    job_results = None
+    if request.args.get("url"):
+        s3_key = url_to_key(request.args.get("url"))
+        prospect = session.query(Prospect).filter_by(s3_key=s3_key).first()
+        jobs = session.query(Job).filter_by(user=prospect.id)
+        job_results = []
+        for job in jobs:
+            print JOB_SQL % ()
+            if job.end_date:
+                job_prospects = session.execute(JOB_SQL %\
+                        (job.company_raw, job.start_date, job.end_date,\
+                        job.start_date, job.end_date))
+            else:
+                job_prospects = session.execute(JOB_SQL %\
+                        (job.company_raw, job.start_date, "2014",\
+                        job.start_date, "2014"))
+            for prospect in job_prospects:
+                result = {}
+                result['name'] = prospect[0]
+                result['company'] = prospect[1]
+                result['start_date'] = prospect[2]
+                result['end_date'] = prospect[3]
+                result['job_location'] = prospect[4]
+                result['current_location'] = prospect[5]
+                result['industry'] = prospect[6]
+                job_results.append(result)
+    return render_template('home.html', job_results=job_results)
 
 
 #linkedin_prospects
