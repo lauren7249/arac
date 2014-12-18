26d86e1cf30024b543456cd1bd09a7f17495b1f1
James Johnson <jamesjohnson11@gmail.com>
2014-12-18 15:25:58 -0500

    added huge update fixing the parser

--- consume/convert.py
+++ consume/convert.py
@@ -1,11 +1,13 @@
 import json
+import lxml.html
+from lxml import etree
 import os
 import re
 import argparse
 import urlparse
 import logging
 
-from bs4 import BeautifulSoup
+#from bs4 import BeautifulSoup, SoupStrainer
 
 profile_re = re.compile('^https?://www.linkedin.com/pub/.*/.*/.*')
 member_re  = re.compile("member-")
@@ -20,31 +22,20 @@ def convert(filename, writefile):
     return True
 
 def main():
-    writefile = open("resuts.json", "a+")
-    os.chdir("data")
-    i = 0
-    logging.info("Creating a pool")
-    pool = GreenPool(size=20)
-    for filename in os.listdir(os.getcwd()):
-        pool.spawn_n(convert, filename, writefile)
-        i += 1
-        sys.stdout.write("\r%.2f%% %s" % (float(i)/10000, i))
-        sys.stdout.flush()
-    pool.waitall()
-
-def debug():
     os.chdir("data")
     for filename in os.listdir(os.getcwd()):
         file = open(filename, 'r').read()
         html = json.loads(file).get("content")
-        soup = BeautifulSoup(html)
-        cleaned = parse_html(html)
-        for school in cleaned.get("schools"):
-            print school.get("end_date"), school.get("start_date"),\
-                    school.get("graduation_date")
+        results =  parse_html(html)
+        #print "File:{0}, Jobs: {1}, Schools:{2}".format(filename,\
+        #        len(results.get("experiences", 0)),
+        #        len(results.get("schools", 0)))
 
-        for job in cleaned.get("experiences"):
-            print job.get("end_date"), job.get("start_date")
+def debug():
+    #file = open("data/http:www.linkedin.compubzachary-kowalski37372872", 'r').read()
+    file = open("data/http:www.linkedin.compubalbie-solis1ab8b34", 'r').read()
+    html = json.loads(file).get("content")
+    return parse_html(html)
 
 def is_profile_link(link):
 
@@ -53,13 +44,7 @@ def is_profile_link(link):
     return False
 
 def get_linked_profiles(html):
-    soup = BeautifulSoup(html)
-    profile_links = filter(is_profile_link, [
-        clean_url(link.get('href')) for link in
-        soup.find_all('a') if link.get('href')
-    ])
-
-    return list(set(profile_links))
+    return list(set(re.findall('https?://www.linkedin.com/pub/.*/.*/.*', html)))
 
 def safe_clean_str(s):
     if s:
@@ -91,175 +76,190 @@ def getnattr(item, attribute, default=None):
         return getattr(item, attribute, default)
     return None
 
-def find_profile_jobs(profile_jobs, soup):
+def find_profile_jobs(raw_html):
     jobs = []
-    for item in profile_jobs.find_all("div", {"class": "position"}):
+    profile_jobs = raw_html.xpath("//div[@id='profile-experience']")[0]
+    for item in profile_jobs.xpath(".//div[contains(@class, 'position ')]"):
         dict_item = {}
-        dict_item["title"] = safe_clean_str(getnattr(item.find("h3"), 'text'))
-        dict_item["company"] = safe_clean_str(getnattr(item.find("h4"), 'text'))
-        dates = item.find_all("abbr")
+        title = item.find(".//h3")
+        if title is not None:
+            dict_item["title"] = safe_clean_str(title.text_content())
+        company = item.find(".//h4")
+        if company is not None:
+            dict_item["company"] = safe_clean_str(company.text_content())
+        dates = item.findall(".//abbr")
         if len(dates) > 1:
             dict_item["start_date"] = dates[0].get('title')
             dict_item["end_date"] = dates[1].get('title')
         if len(dates) == 1:
+            import pdb
+            pdb.set_trace()
             dict_item["start_date"] = dates[0].get('text')
             dict_item["end_date"] = "Present"
-        dict_item['location'] = safe_clean_str(getnattr(item.find("span",\
-            {"class": "location"}), 'text'))
-        dict_item["description"] = safe_clean_str(getnattr(item.find("p",\
-                class_='description'), 'text'))
-
+        location = item.xpath(".//span[@class='location']")
+        if len(location) > 0:
+            dict_item['location'] = safe_clean_str(location[0].text_content())
+        description = item.xpath(".//p[contains(@class, ' description ')]")
+        if len(description) > 0:
+            dict_item["description"] = safe_clean_str(description[0].text_content())
         jobs.append(dict_item)
     return jobs
 
-def find_background_jobs(background_jobs):
+def find_background_jobs(raw_html):
     jobs = []
-    for item in background_jobs.find_all("div"):
+    background_jobs = raw_html.xpath("//div[@id='background-experience']/div")
+    for item in background_jobs:
         dict_item = {}
-        dict_item["title"] = safe_clean_str(getnattr(item.find("h4"), 'text'))
-        company = item.find_all("h5")
+        title = item.find(".//h4")
+        if title is not None:
+            dict_item["title"] = safe_clean_str(title.text_content())
+        company = item.findall(".//h5")
         if len(company) == 2:
-            dict_item["company"] = safe_clean_str(getnattr(company[1], 'text'))
+            dict_item["company"] = safe_clean_str(company[1].text_content())
         else:
-            dict_item["company"] = safe_clean_str(getnattr(company[0], 'text'))
-        dict_item['location'] = safe_clean_str(getnattr(item.find("span",\
-            {"class": "locality"}), 'text'))
-        dict_item["description"] = safe_clean_str(getnattr(item.find("p",\
-                class_='description'), 'text'))
-        dates = item.find_all("time")
+            dict_item["company"] = safe_clean_str(company[0].text_content())
+        location = item.xpath(".//span[@class='locality']")
+        if len(location) > 0:
+            dict_item['location'] = safe_clean_str(location[0].text_content())
+        description = item.xpath(".//p[contains(@class, 'description')]")
+        if len(description) > 0:
+            dict_item["description"] = safe_clean_str(description[0].text_content())
+        dates = item.findall(".//time")
         if len(dates) > 1:
-            dict_item["start_date"] = getnattr(dates[0], 'text')
-            dict_item["end_date"] = getnattr(dates[1], 'text')
+            dict_item["start_date"] = safe_clean_str(dates[0].text_content())
+            dict_item["end_date"] = safe_clean_str(dates[1].text_content())
         if len(dates) == 1:
-            dict_item["start_date"] = getnattr(dates[0], 'text')
+            dict_item["start_date"] = safe_clean_str(dates[0].text_content())
             dict_item["end_date"] = "Present"
-
         jobs.append(dict_item)
     return jobs
 
-def find_jobs(soup):
-    jobs = []
-    profile_jobs = soup.find(id="profile-experience")
-    background_jobs = soup.find(id="background-experience")
-    if profile_jobs:
-        jobs = find_profile_jobs(profile_jobs, soup)
-    if background_jobs:
-        jobs = find_background_jobs(background_jobs)
 
-    return jobs
-
-def find_profile_schools(profile_schools):
+def find_profile_schools(raw_html):
     schools = []
-    for item in profile_schools.find_all("div", {"class": "position"}):
+    profile_jobs = raw_html.xpath("//div[@id='profile-education']")[0]
+    for item in profile_jobs.xpath(".//div[contains(@class, 'position ')]"):
         dict_item = {}
-        dict_item["college"] = safe_clean_str(getnattr(item.find("h3"), 'text'))
-        dict_item["degree"] = safe_clean_str(getnattr(item.find("h4"), 'text'))
-        dates = item.find_all("abbr")
+        college = item.find(".//h3")
+        if college is not None:
+            dict_item["college"] = safe_clean_str(college.text_content())
+        degree = item.find(".//h4")
+        if degree is not None:
+            dict_item["degree"] = safe_clean_str(degree.text_content())
+        dates = item.findall(".//abbr")
         if len(dates) > 1:
             dict_item["start_date"] = dates[0].get('title')
             dict_item["end_date"] = dates[1].get('title')
-        if len(dates) == 1 and not "Present" in item:
+
+        present = "Present" in etree.tostring(item, pretty_print=True)
+        if len(dates) == 1 and not present:
             dict_item["graduation_date"] = dates[0].get('title')
-        if len(dates) == 1 and "Present" in item:
+        if len(dates) == 1 and present:
             dict_item["start_date"] = dates[0].get('title')
             dict_item["end_date"] = "Present"
 
-        dict_item["description"] = safe_clean_str(getnattr(item.find("p.description"), 'text'))
+        description = item.xpath(".//p[contains(@class, 'desc ')]")
+        if len(description) > 0:
+            dict_item["description"] = safe_clean_str(description[0].text_content())
         schools.append(dict_item)
     return schools
 
-def find_background_schools(background_schools):
+def find_background_schools(raw_html):
     schools = []
-    for item in background_schools.find_all("div"):
+    background_schools = raw_html.xpath("//div[@id='background-education']/div")
+    for item in background_schools:
         dict_item = {}
-        dict_item["college"] = safe_clean_str(getnattr(item.find("h4"), 'text'))
-        degrees = item.find_all("h5")
+        college = item.find(".//h4")
+        if college is not None:
+            dict_item["college"] = safe_clean_str(college.text_content())
+        degrees = item.findall(".//h5")
         if len(degrees) == 2:
-            dict_item["degree"] = safe_clean_str(getnattr(degrees[1], 'text'))
+            dict_item["degree"] = safe_clean_str(degrees[1].text_content())
         if len(degrees) == 1:
-            dict_item["degree"] = safe_clean_str(getnattr(degrees[0], 'text'))
-        dates = item.find_all("time")
+            dict_item["degree"] = safe_clean_str(degrees[0].text_content())
+        dates = item.findall("time")
         if len(dates) > 1:
-            dict_item["start_date"] = getnattr(dates[0], 'text')
-            dict_item["end_date"] = safe_clean_str(getnattr(dates[1], 'text').encode('ascii', 'ignore'))
-        if len(dates) == 1 and not "Present" in item:
-            dict_item["graduation_date"] = getnattr(dates[0], 'text')
-        if len(dates) == 1 and "Present" in item:
-            dict_item["start_date"] = getnattr(dates[0], 'text')
+            dict_item["start_date"] = safe_clean_str(dates[0].text_content())
+            dict_item["end_date"] = safe_clean_str(dates[1].text_content().encode('ascii', 'ignore'))
+
+        present = "Present" in etree.tostring(item, pretty_print=True)
+        if len(dates) == 1 and not present:
+            dict_item["graduation_date"] = dates[0].text_content()
+        if len(dates) == 1 and present:
+            dict_item["start_date"] = dates[0].text_content()
             dict_item["end_date"] = "Present"
 
-        dict_item["description"] = safe_clean_str(getnattr(item.find("p.description"), 'text'))
+        description = item.xpath(".//p[contains(@class, ' desc ')]")
+        if len(description) > 0:
+            dict_item["description"] = safe_clean_str(description[0].text_content())
         schools.append(dict_item)
     return schools
 
-def find_schools(soup):
-    schools = []
-
-    #Linkedin has two different kinds of public pages HTML
-    profile_schools = soup.find(id="profile-education")
-    background_schools = soup.find(id="background-education")
-    if profile_schools:
-        schools = find_profile_schools(profile_schools)
-    if background_schools:
-        schools = find_background_schools(background_schools)
-    return schools
 
 def parse_html(html):
-    #TODO put back in 'lxml'
-    soup = BeautifulSoup(html, 'lxml')
+    raw_html = lxml.html.fromstring(html)
 
     full_name = None
-    full_name_el = soup.find(class_='full-name')
-    if full_name_el:
-        full_name = full_name_el.text.strip()
     try:
-        div = soup.find("div", id=member_re)
-        linkedin_id = div.get("id").split("-")[1]
-        if len(linkedin_id) < 3:
-            linkedin_index = str(soup).find("newTrkInfo='") + 12
-            end_index = str(soup)[linkedin_index:].find("'")
-            linkedin_id = str(soup)[linkedin_index:linkedin_index + end_index].replace(",", "")
+        full_name = raw_html.xpath("//span[@class='full-name']")[0].text_content()
     except:
-        try:
-            linkedin_index = str(soup).find("newTrkInfo='") + 12
-            end_index = str(soup)[linkedin_index:].find("'")
-            linkedin_id = str(soup)[linkedin_index:linkedin_index+end_index].replace(",", "")
-        except:
-            linkedin_id = None
+        pass
 
     try:
-        all_dd = soup.find("div", id='location').find_all("dd")
+        linkedin_index = html.find("newTrkInfo='") + 12
+        end_index = html[linkedin_index:].find("'")
+        linkedin_id = html[linkedin_index:linkedin_index+end_index].replace(",", "")
+    except:
+        linkedin_id = None
+
+    location = None
+    industry = None
+    try:
+        all_dd = raw_html.xpath("//div[@id='location']/dd")
         location = all_dd[0].text
         industry = all_dd[1].text
     except:
         try:
-            location = soup.find("span", {'class': 'locality'}).text
-            industry = soup.find("dd", {'class': "industry"}).text
+            location = raw_html.xpath("//span[@class='locality']")[0].text
+            industry = raw_html.xpath("//dd[@class='industry']")[0].text
         except:
-            location = None
-            industry = None
+            pass
+
 
     location = safe_clean_str(location)
     industry = safe_clean_str(industry)
 
+    connections = None
     try:
-        connections = soup.find("div", {"class": "member-connections"}).text.split("connections")[0]
+        connections = raw_html.xpath("//div[@class='member-connections']").text_content()
         connections = "".join(re.findall("\d+", connections))
     except:
         try:
-            connections = soup.find("dd", {'class': "overview-connections"}).text
+            connections = raw_html.xpath("//dd[@class='overview-connections']")[0].text_content()
             connections = "".join(re.findall("\d+", connections))
         except:
-            connections = None
+            pass
+
+    experiences = []
+    schools = []
+    if len(raw_html.xpath("//div[@id='profile-experience']")) > 0:
+        experiences = find_profile_jobs(raw_html)
+
+    if len(raw_html.xpath("//div[@id='background-experience']")) > 0:
+        experiences = find_background_jobs(raw_html)
+
+    school_type = None
+    if len(raw_html.xpath("//div[@id='profile-education']")) > 0:
+        schools = find_profile_schools(raw_html)
+
+    if len(raw_html.xpath("//div[@id='background-education']")) > 0:
+        experiences = find_background_schools(raw_html)
 
 
-    experiences = find_jobs(soup)
-
-    schools = find_schools(soup)
-
-    skills = [e.text for e in soup.find_all("li", {'class': 'endorse-item'})]
+    skills = [e.text_content() for e in raw_html.xpath("//li[@class='endorse-item']")]
     people = get_linked_profiles(html)
 
+    print experiences
     return {
         'linkedin_id': linkedin_id,
         'full_name': full_name,
@@ -275,6 +275,14 @@ def parse_html(html):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--noresults')
+    parser.add_argument('--benchmark', action='store_true')
     args = parser.parse_args()
-    debug()
+
+    if args.benchmark:
+        import timeit
+        print(timeit.timeit("debug()", setup="from __main__ import debug",
+            number=10000))
+    else:
+        main()
+
+
