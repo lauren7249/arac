b1b6e3e12479b437c49f3f16450a179abce48c85
James Johnson <jamesjohnson11@gmail.com>
2015-01-05 22:24:42 -0500

    added view fix

--- prime/prospects/views.py
+++ prime/prospects/views.py
@@ -31,19 +31,19 @@ def search():
     return render_template('home.html', results=results)
 
 @prospects.route("/company/<int:company_id>")
-def company():
+def company(company_id):
     company = session.query(Company).get(company_id)
     prospects = session.query(Prospect).filter_by(company=company)
     return render_template('company.html', company=company, prospects=prospects)
 
 @prospects.route("/company/<int:school_id>")
-def school():
+def school(school_id):
     school = session.query(School).get(school_id)
     prospects = session.query(Prospect).filter_by(school=school)
     return render_template('school.html', school=school, prospects=prospects)
 
 @prospects.route("/prospect/<int:prospect_id>")
-def prospect():
+def prospect(prospect_id):
     prospect = session.query(Prospect).get(prospect_id)
     jobs = prospect.jobs
     schools = prospect.schools
