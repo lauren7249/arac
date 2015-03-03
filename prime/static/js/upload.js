4296448084087523e3219a0a34a42e9d42f97d16
James Johnson <jamesjohnson11@gmail.com>
2015-03-02 22:28:43 -0500

    cross origin domain

--- prime/static/js/upload.js
+++ prime/static/js/upload.js
@@ -1,16 +1,31 @@
 function selectProfile(url) {
     $(".overlay").show();
-    var params={url:url}
+
+    var item = choose(['http://54.152.186.2', 'http://54.152.181.248'])
+
     try {
-        $.post("/select", params, function(data) {
-            if (data.success) {
-                window.location = "/confirm"
+        $.ajax({
+            url: item + ":9090/proxy?url=" + url,
+            dataType: 'jsonp',
+            jsonpCallback: 'redirect',
+            error:function(){
+                alert("Error");
             }
-        });
+        })
+
     } catch(err) {
         $(".overlay").hide();
         alert("Whoops, something went wrong. Please try again");
     }
+}
+
+var redirect = function(data) {
+    alert(data.prospect_url)
+}
+
+function choose(choices) {
+  var index = Math.floor(Math.random() * choices.length);
+  return choices[index];
 }
 
 $(function() {
