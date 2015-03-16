442c23690f28eca664045a95b074d4913b3e0971
Brett Jurman <i.be.brett@gmail.com>
2015-03-16 16:15:47 +0000

    added view logic

--- prime/prospects/views.py
+++ prime/prospects/views.py
@@ -137,7 +137,7 @@ def confirm_profile():
 
 @prospects.route("/dashboard")
 def dashboard():
-    page = request.args.get("p", 0)
+    page = int(request.args.get("p", 0))
     first_time = False
     if 'first_time' in flask_session:
         first_time = True
