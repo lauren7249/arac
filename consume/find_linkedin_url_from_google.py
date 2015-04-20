848a37e2bf220784651bdda4184b7cb806794a56
James Johnson <jamesjohnson11@gmail.com>
2015-04-20 15:34:39 -0400

    added linkedin url finder

---
+++ consume/find_linkedin_url_from_google.py
@@ -0,0 +1,25 @@
+import requests
+import argparse
+import lxml.html
+
+URL = "https://www.google.com/search?q={}&oq={}&aqs=chrome..69i57.6893j0j1&sourceid=chrome&es_sm=91&ie=UTF-8"
+headers ={'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+
+def find_linkedin_url(url):
+    query = "site:linkedin.com+" + url
+    LINKEDIN_URL = "https://www.google.com/search?q={}&oq={}&aqs=chrome..69i57.6893j0j1&sourceid=chrome&es_sm=91&ie=UTF-8"
+    result = requests.get(LINKEDIN_URL.format(query, query), headers=headers)
+    crawlable = lxml.html.fromstring(result.content)
+    links = [ad.xpath("./@href")[0] \
+            for ad in crawlable.xpath("//h3/a") \
+            if "google" not in ad.xpath("./@href")[0]]
+    for link in links:
+        if 'company' in link:
+            print link
+            return link
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('url')
+    args = parser.parse_args()
+    find_linkedin_url(args.url)
