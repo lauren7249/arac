963d14110925c83772945148b7796abf9d4c01ff
lauren talbot <lauren@laurens-MacBook-Pro.local>
2015-05-21 13:11:00 -0400

    needed to import datetime

--- prime/utils/scrape_proxies.py
+++ prime/utils/scrape_proxies.py
@@ -3,6 +3,7 @@ import requests
 import os, re
 from geoip import geolite2
 import redis
+import datetime
 
 ua='Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'
 headers ={'User-Agent':ua}
