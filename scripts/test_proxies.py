406bab933b2657f2e2f5a9e0cf3e00674673ef2f
lauren talbot <lauren@laurens-MacBook-Pro.local>
2015-05-21 13:33:43 -0400

    testing only promising proxies

--- scripts/test_proxies.py
+++ scripts/test_proxies.py
@@ -4,14 +4,14 @@ import datetime
 from consume.convert import parse_html
 import multiprocessing
 
-if r.llen("untested_proxies")>r.scard("urls") and False:
-	add_urls(filename="/Users/lauren/test_urls.txt", limit=r.llen("untested_proxies"))
+if r.llen("untested_promising_proxies")>r.scard("urls") and False:
+	add_urls(filename="/Users/lauren/test_urls.txt", limit=r.llen("untested_promising_proxies"))
 timeout = 5
-n_processes = 2000
+n_processes = 500
 
 def work():
 	while True:
-		raw=r.rpop("untested_proxies")
+		raw=r.rpop("untested_promising_proxies")
 		if raw is None: break
 		time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 		d = eval(raw)
@@ -29,26 +29,26 @@ def work():
 		defunct = response is None
 		d.update({"timeout":timeout,"defunct":defunct, "time_checked":time})
 		if defunct: 
-			r.rpush("tested_proxies",d)
+			r.rpush("tested_promising_proxies",d)
 			continue
 		ip = get_ip(proxy)
 		match = geolite2.lookup(ip)
 		d.update({"country":match.country,"continent":match.continent, "response_code":response.status_code, "response_headers": response.headers})
 		if response.status_code != 200:
-			r.rpush("tested_proxies",d)
+			r.rpush("tested_promising_proxies",d)
 			continue
 		info = parse_html(response.content)
 		d.update({"redirect": not info.get("success")}) 
 		if not info.get("success"):
 			d.update({"html": response.content})
-			r.rpush("tested_proxies",d)
+			r.rpush("tested_promising_proxies",d)
 			continue	
 		d.update({"incomplete": not info.get("complete")}) 	 
 		if not info.get("complete"):
 			d.update({"html": response.content})
-			r.rpush("tested_proxies",d)
+			r.rpush("tested_promising_proxies",d)
 			continue			
-		r.rpush("tested_proxies",d)
+		r.rpush("tested_promising_proxies",d)
 
 pool = multiprocessing.Pool(n_processes)
 for i in xrange(0, n_processes):
