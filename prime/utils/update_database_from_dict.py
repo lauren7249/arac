032d35d7d0e4c456cbf41fe2c574816108d6428f
lauren talbot <lauren@laurens-MacBook-Pro.local>
2015-05-23 04:26:39 -0400

    fixing session issues

--- prime/utils/update_database_from_dict.py
+++ prime/utils/update_database_from_dict.py
@@ -4,9 +4,7 @@ from sqlalchemy.orm import joinedload
 from consume.consumer import update_prospect_from_info, create_prospect_from_info
 from prime.prospects.get_prospect import get_session
 
-session = get_session()
-
-def insert_linkedin_profile(info):
+def insert_linkedin_profile(info, session):
 	linkedin_id = info.get("linkedin_id")
 	prospect = session.query(models.Prospect).filter_by(linkedin_id=linkedin_id).options(joinedload(models.Prospect.schools).joinedload(models.Education.school), joinedload(models.Prospect.jobs).joinedload(models.Job.company)).first()
 	if prospect: 
