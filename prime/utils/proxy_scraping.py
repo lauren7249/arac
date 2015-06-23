716b4360afb460a06e3e3b96bb4e82268b563286
lauren talbot <laurentracytalbot@gmail.com>
2015-06-23 18:31:59 -0400

    finally finished proxy query

--- prime/utils/proxy_scraping.py
+++ prime/utils/proxy_scraping.py
@@ -7,6 +7,7 @@ from prime.prospects.get_prospect import session
 from prime.prospects.models import Proxy, ProxyDomainStatus
 from datetime import datetime, timedelta
 from sqlalchemy import *
+from random import randint
 
 requests.packages.urllib3.disable_warnings()
 
@@ -58,7 +59,7 @@ def record_failure(proxy, domain, response):
 		proxy.last_success = datetime.utcnow()
 		proxy.consecutive_timeouts=0
 		status = session.query(ProxyDomainStatus).get((proxy.url,domain))
-		if status is None: status = ProxyDomainStatus(proxy_url=proxy.url, domain=domain)
+		if status is None: status = ProxyDomainStatus(proxy_url=proxy.url, domain=domain, last_rejected=datetime.fromtimestamp(0), last_accepted=datetime.fromtimestamp(0))
 		status.last_rejected = datetime.utcnow()
 		session.add(status)	
 	session.add(proxy)	
@@ -70,7 +71,7 @@ def record_success(proxy, domain):
 	proxy.consecutive_timeouts=0
 	session.add(proxy)
 	status = session.query(ProxyDomainStatus).get((proxy.url,domain))
-	if status is None: status = ProxyDomainStatus(proxy_url=proxy.url, domain=domain)
+	if status is None: status = ProxyDomainStatus(proxy_url=proxy.url, domain=domain, last_rejected=datetime.fromtimestamp(0), last_accepted=datetime.fromtimestamp(0))
 	status.last_accepted = datetime.utcnow()
 	session.add(status)	
 	session.flush()
@@ -78,7 +79,18 @@ def record_success(proxy, domain):
 
 #return Proxy object
 def pick_proxy(domain):
-	return session.query(Proxy).order_by(desc(Proxy.last_success)).filter(and_(or_(Proxy.last_timeout < (datetime.utcnow() - timedelta(days=try_again_after_timeout_days)), Proxy.last_timeout ==None, Proxy.last_success > Proxy.last_timeout), Proxy.consecutive_timeouts < max_consecutive_timeouts)).first()
+	#keep wait time random
+	wait_btwn_requests_seconds = randint(min_wait_btwn_requests_seconds, max_wait_btwn_requests_seconds)
+	rejects = session.query(ProxyDomainStatus.proxy_url).filter(and_(domain==ProxyDomainStatus.domain, 
+	or_(ProxyDomainStatus.last_rejected >= (datetime.utcnow() - timedelta(days=try_again_after_reject_days)),  #rejected by the domain recently
+		ProxyDomainStatus.last_rejected >= (datetime.utcnow() - timedelta(seconds=wait_btwn_requests_seconds)), #tried very recently
+		ProxyDomainStatus.last_accepted >= (datetime.utcnow() - timedelta(seconds=wait_btwn_requests_seconds))
+	)))
+	return session.query(Proxy).order_by(desc(Proxy.last_success)).filter(and_(or_( #pick the most recently non-defunct proxy
+		Proxy.last_timeout < (datetime.utcnow() - timedelta(days=try_again_after_timeout_days)),  #hasn't timed out in the last 2 days
+			Proxy.last_success > Proxy.last_timeout), #or we had a success more recently than the timeout
+		Proxy.consecutive_timeouts < max_consecutive_timeouts, #but we aren't going to keep trying if it keeps timing out over and over
+		~Proxy.url.in_(rejects))).first() #not on the rejects list
 
 def robust_get_url(url, expected_xpath):
 	successful, response = try_request(url, expected_xpath)
