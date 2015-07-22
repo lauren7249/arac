b58b75633881419aefe19ed7d61240359708db57
lauren talbot <laurentracytalbot@gmail.com>
2015-07-21 21:28:08 -0400

    created normalization process for school names

--- prime/utils/googling.py
+++ prime/utils/googling.py
@@ -6,6 +6,7 @@ def search(querystring, results_per_page=100, start_num=0, limit=1000000, url_re
 	urls = set()
 	while True:
 		search_query ="http://www.google.com/search?q=" + querystring + secret_sauce + "&num=" + str(results_per_page) + "&start=" + str(start_num) 
+		print search_query
 		raw_html = robust_get_url(search_query, "//*[contains(@class,'srg')]", require_proxy=require_proxy)
 		if raw_html is None: break
 		results_area = raw_html.xpath("//*[contains(@class,'srg')]")
@@ -17,8 +18,41 @@ def search(querystring, results_per_page=100, start_num=0, limit=1000000, url_re
 		start_num+=results_per_page
 	return urls
 
+def search_with_title(querystring, results_per_page=100, start_num=0, limit=1000000, url_regex=".", require_proxy=False):
+	results = {}
+	while True:
+		search_query ="http://www.google.com/search?q=" + querystring + secret_sauce + "&num=" + str(results_per_page) + "&start=" + str(start_num) 
+		raw_html = robust_get_url(search_query, "//*[contains(@class,'srg')]", require_proxy=require_proxy)
+		if raw_html is None: break
+		results_area = raw_html.xpath("//*[contains(@class,'srg')]")
+		links = results_area[0].xpath("//h3[@class='r']/a")
+		for linkel in links:
+			link = linkel.values()[0]
+			if re.search(url_regex,link): 
+				title = linkel.text
+				results.update({link: title})
+			if limit == len(results): return results
+		start_num+=results_per_page
+	return results
+
 def search_linkedin_profile(terms, limit=10, require_proxy=False):
 	profile_re = re.compile('(^https?://www.linkedin.com/pub/((?!dir).)*/.*/.*)|(^https?://www.linkedin.com/in/.*)')
 	querystring = "site%3Awww.linkedin.com+" + re.sub(r" ", "+", terms)
 	urls = search(querystring, url_regex=profile_re, limit=limit, require_proxy=require_proxy)
 	return urls
+
+def search_linkedin_school(terms, limit=1, require_proxy=False):
+	profile_re = re.compile('^https://www.linkedin.com/edu/*')
+	querystring = "site%3Awww.linkedin.com+" + re.sub(r" ", "+", terms)
+	results = search_with_title(querystring, url_regex=profile_re, limit=limit, require_proxy=require_proxy)
+	return results
+
+def normalize_school_name(name):
+	try:
+		u = search_linkedin_school(name)
+		title = u.values()[0]
+		name = title.split("|")[0].strip()
+		return name
+	except:
+		return None
+
