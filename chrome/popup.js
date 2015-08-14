24ce4cbb5fef8326dfd4ee428ab01ceea63901ca
Michael Bishop <mbishop@aquariusinteractive.com>
2015-08-14 16:52:34 -0400

    Add  documentation to existing functions

--- chrome/popup.js
+++ chrome/popup.js
@@ -1,10 +1,11 @@
 var xmlHttp
 var total = 0;
+var countArea;
+
 //TODO: stop if logged in
 //TODO: show captcha if it comes up
 
 function initContentSettings() {
-    console.debug('initContentSettings');
 
     chrome.contentSettings.location.set({
         primaryPattern: '<all_urls>',
@@ -16,22 +17,30 @@ function initContentSettings() {
     });
 }
 
-function get_url(orig_url, countArea) {
+/**
+ * Retrieve the content of a url
+ * @param orig_url
+ * @returns {boolean}
+ *
+ * TODO Assumes all urls have an https:// is this as safe assumption?
+ * FIXME Gracefully handle captcha
+ */
+function get_url(orig_url) {
     var url = orig_url.replace("https://", "").replace("http://", "");
     console.log(url);
     var fn = url.replace(/\//g, "-") + ".html";
     get_url_response("https://" + url, function () {
 
-    })
+    });
     try {
         var page = get_url_response("https://" + url);
         if (page.toLowerCase().indexOf("captcha") > -1) {
-            console.log("captcha")
+            console.log("captcha");
             return false
         }
     }
     catch (err) {
-        console.log("not loaded")
+        console.log("not loaded");
         return false
     }
     //console.log(page);
@@ -47,6 +56,15 @@ function get_url(orig_url, countArea) {
     return true
 }
 
+/**
+ * Make an asynchronous HTTP call and execute the
+ * callback on success
+ *
+ * @param url
+ * @param callback function on success
+ *
+ * TODO Error callback
+ */
 function get_url_response(url, callback) {
     xmlHttp = new XMLHttpRequest();
     xmlHttp.onreadystatechange = callback;
@@ -54,8 +72,6 @@ function get_url_response(url, callback) {
     xmlHttp.setRequestHeader('Cache-Control', 'no-cache');
     xmlHttp.setRequestHeader('Accept-Language', 'en-US');
     xmlHttp.send(null);
-
-    return xmlHttp.responseText;
 }
 
 function is_google(url) {
@@ -66,14 +82,25 @@ function google(url) {
     console.log("is google");
 }
 
-// Initialize the Amazon Cognito credentials provider
-AWS.config.region = 'us-east-1'; // Region
-AWS.config.credentials = new AWS.CognitoIdentityCredentials({
-    IdentityPoolId: 'us-east-1:d963e11a-7c9b-4b98-8dfc-8b2a9d275574',
-});
-var bucket = new AWS.S3({params: {Bucket: 'chrome-ext-uploads'}});
+/**
+ * Initialize the Amazon Cognito credentials provider
+ */
+function initAWS() {
+    AWS.config.region = 'us-east-1'; // Region
+    AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+        IdentityPoolId: 'us-east-1:d963e11a-7c9b-4b98-8dfc-8b2a9d275574',
+    });
+    var bucket = new AWS.S3({params: {Bucket: 'chrome-ext-uploads'}});
+}
 
+/**
+ * Callback function that checks that xmlHttpRequest has completed
+ * and runs a callback on success.
+ *
+ * @param callback Function to run on success
+ */
 function onHttpComplete(callback) {
+    console.debug(xmlHttp.status);
     if (xmlHttp.readyState == XMLHttpRequest.DONE && xmlHttp.status == 200) {
         callback();
     } else if (xmlHttp.readyState = XMLHttpRequest.DONE && xmlHttp.status != 200) {
@@ -82,6 +109,13 @@ function onHttpComplete(callback) {
     }
 }
 
+/**
+ * Selects all outstanding urls and processes up to 100 (I think)
+ * Function not called -- maybe used for testing else eligible for
+ * deletion.
+ *
+ * TODO Is this used?
+ */
 function infinite() {
 
     var k = 0, url = null;
@@ -98,7 +132,6 @@ function infinite() {
                 }
             });
         });
-        //url = get_url_response("http://169.55.28.212:8080/select")
         k += 1;
         if (k > 100) {
             break;
@@ -108,9 +141,12 @@ function infinite() {
     alert("done!")
 }
 
-
+/**
+ * Initialization function executed after DOM is loaded
+ */
 document.addEventListener('DOMContentLoaded', function () {
     initContentSettings();
+    initAWS();
     var checkPageButton = document.getElementById('checkPage');
     var countArea = document.getElementById('count');
     checkPageButton.addEventListener('click', function () {
@@ -126,7 +162,7 @@ document.addEventListener('DOMContentLoaded', function () {
                     countArea.max = arr.length;
                     for (var i in arr) {
                         url = arr[i];
-                        success = get_url(url, countArea);
+                        success = get_url(url);
                         if (!success) {
                             break
                         }
