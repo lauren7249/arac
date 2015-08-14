608f87f2ba8e0bdff6730a299802000166a44d17
Michael Bishop <mbishop@aquariusinteractive.com>
2015-08-14 04:26:29 -0400

    Formatting.

--- chrome/popup.js
+++ chrome/popup.js
@@ -2,7 +2,7 @@ var total = 0;
 //TODO: stop if logged in
 //show captcha if it comes up
 
-function initContentSettings(){
+function initContentSettings() {
     chrome.contentSettings['location'].set({
         primaryPattern: '<all_urls>',
         setting: 'block'
@@ -14,47 +14,47 @@ function initContentSettings(){
 }
 
 function get_url(orig_url) {
-	url = orig_url.replace("https://","").replace("http://","");
-	console.log(url);
-	var fn = url.replace(/\//g, "-") + ".html";
-	try {
-		var page = get_url_response("https://" + url);
-		if (page.toLowerCase().indexOf("captcha") > -1) {
-			console.log ("captcha")
-			return false
-		}
-	}
-	catch(err) {
-		console.log("not loaded")
-		return false
-	}
-	//console.log(page);
-	var params = {Key: fn, ContentType:'text/html', Body: page};
-	bucket.upload(params, function (err, data) {
-		var xmlHttp = new XMLHttpRequest();
-		xmlHttp.open( "GET", "http://169.55.28.212:8080/log_uploaded/url=" + orig_url.replace(/\//g, ";").replace(/\?/g, "`"), false );
-		xmlHttp.send( null );	
-		total += 1;
-		countArea.value = total;
-		return true
-	}); 	
-	return true
+    url = orig_url.replace("https://", "").replace("http://", "");
+    console.log(url);
+    var fn = url.replace(/\//g, "-") + ".html";
+    try {
+        var page = get_url_response("https://" + url);
+        if (page.toLowerCase().indexOf("captcha") > -1) {
+            console.log("captcha")
+            return false
+        }
+    }
+    catch (err) {
+        console.log("not loaded")
+        return false
+    }
+    //console.log(page);
+    var params = {Key: fn, ContentType: 'text/html', Body: page};
+    bucket.upload(params, function (err, data) {
+        var xmlHttp = new XMLHttpRequest();
+        xmlHttp.open("GET", "http://169.55.28.212:8080/log_uploaded/url=" + orig_url.replace(/\//g, ";").replace(/\?/g, "`"), false);
+        xmlHttp.send(null);
+        total += 1;
+        countArea.value = total;
+        return true
+    });
+    return true
 }
 
 function get_url_response(url) {
-	var xmlHttp = new XMLHttpRequest();
-	xmlHttp.open( "GET", url, false );
-	xmlHttp.send( null );
-	var page = xmlHttp.responseText;	
-	return page;
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("GET", url, false);
+    xmlHttp.send(null);
+    var page = xmlHttp.responseText;
+    return page;
 }
 
 function is_google(url) {
-	return url.match(/www.google.com/g, url) != null;
+    return url.match(/www.google.com/g, url) != null;
 }
 
 function google(url) {
-	console.log("is google");
+    console.log("is google");
 }
 
 // Initialize the Amazon Cognito credentials provider
@@ -65,52 +65,55 @@ AWS.config.credentials = new AWS.CognitoIdentityCredentials({
 var bucket = new AWS.S3({params: {Bucket: 'chrome-ext-uploads'}});
 
 function infinite() {
-	var k = 0
-	while(true) {
-		url = get_url_response("http://169.55.28.212:8080/select")
-		if  (url.match(/www./g, url) != null) {
-			get_url(url);
-		} 
-		else {
-			break;
-		}
-		k += 1;
-		if (k > 100){
-			break;
-		} 
-	}
-	k = 0;
 
-	alert("done!")	
+    var k = 0, url = null;
+
+    while (true) {
+        url = get_url_response("http://169.55.28.212:8080/select")
+        if (url.match(/www./g, url) != null) {
+            get_url(url);
+        }
+        else {
+            break;
+        }
+        k += 1;
+        if (k > 100) {
+            break;
+        }
+    }
+    k = 0;
+
+    alert("done!")
 }
 
 
-document.addEventListener('DOMContentLoaded', function() {
-  var checkPageButton = document.getElementById('checkPage');
-  countArea = document.getElementById('count');
-  checkPageButton.addEventListener('click', function() {
- 	var url_field = document.getElementById('query').value;
-	if (url_field.length == 0) {
-		url_field = get_url_response("http://169.55.28.212:8080/select/n=100")
-	}   	
+document.addEventListener('DOMContentLoaded', function () {
+    initContentSettings();
+    var checkPageButton = document.getElementById('checkPage');
+    countArea = document.getElementById('count');
+    checkPageButton.addEventListener('click', function () {
+        var url_field = document.getElementById('query').value;
+        if (url_field.length == 0) {
+            url_field = get_url_response("http://169.55.28.212:8080/select/n=100")
+        }
 
-	arr = url_field.match(/[^\r\n]+/g);
-	countArea.max = arr.length;
-	for (var i in arr) {
-		url = arr[i]
-		if (false) {
-			google(url);
-		}
-		else {
-			success = get_url(url);
-			if (!success) { 
-				break 
-			}
-		}
-	}
+        arr = url_field.match(/[^\r\n]+/g);
+        countArea.max = arr.length;
+        for (var i in arr) {
+            url = arr[i]
+            if (false) {
+                google(url);
+            }
+            else {
+                success = get_url(url);
+                if (!success) {
+                    break
+                }
+            }
+        }
 
-	
-  }, false);
+
+    }, false);
 }, false);
 
 
