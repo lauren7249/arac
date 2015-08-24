834bc17e2b7fc22c707e62522dc10d52826447ae
Michael Bishop <mbishop@aquariusinteractive.com>
2015-08-24 11:22:51 -0400

    Delete fiels

---
+++ chrome_tng/react-chrome-extension-boilerplate/app/containers/TodoApp.js
@@ -0,0 +1,34 @@
+import React, { Component, PropTypes } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import Header from '../components/Header';
+import MainSection from '../components/MainSection';
+import * as TodoActions from '../actions/TodoActions';
+
+class TodoApp extends Component {
+  render() {
+    const { todos, dispatch } = this.props;
+    console.log(this.props);
+    const actions = bindActionCreators(TodoActions, dispatch);
+
+    return (
+      <div>
+        <Header addTodo={actions.addTodo} />
+        <MainSection todos={todos} actions={actions} />
+      </div>
+    );
+  }
+}
+
+TodoApp.propTypes = {
+  todos: PropTypes.array.isRequired,
+  dispatch: PropTypes.func.isRequired
+};
+
+function select(state) {
+  return {
+    todos: state.todos
+  };
+}
+
+export default connect(select)(TodoApp);
