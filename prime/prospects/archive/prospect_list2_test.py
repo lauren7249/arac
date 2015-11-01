e0c30122a3b06f19acaac31c565f4c98881641c1
James Johnson <jamesjohnson11@gmail.com>
2015-11-01 12:38:18 -0500

    removed archive

--- prime/prospects/archive/prospect_list2_test.py
+++
@@ -1,8 +0,0 @@
-from prime.prospects.models import db, Prospect
-from prime.prospects.prospect_list2 import ProspectList
-
-session = db.session
-url = "http://www.linkedin.com/pub/jimmy-lyons/41/7b9/968"
-prospect = session.query(Prospect).filter_by(s3_key=url.replace("/", "")).first()
-plist = ProspectList(prospect)
-plist.get_results()
