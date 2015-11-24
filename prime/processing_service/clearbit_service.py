c11093c850f2d7607f9f460ba8e2721e0048b970
lauren talbot <laurentracytalbot@gmail.com>
2015-11-23 19:23:44 -0500

    added merge functionality for social accounts and linkedin url

--- prime/processing_service/clearbit_service.py
+++ prime/processing_service/clearbit_service.py
@@ -3,14 +3,14 @@ import logging
 import hashlib
 import boto
 import multiprocessing
+import re
 from requests import HTTPError
 from boto.s3.key import Key
 from helper import get_domain
 from service import Service, S3SavedRequest
+from prime.processing_service.constants import pub_profile_re
 
 def unwrap_process_person(person):
-    if len(person.values()) > 0 and person.values()[0].get("linkedin_urls"):
-        return person
     email = person.keys()[0]
     request = ClearbitRequest(email)
     data = request.get_person()
@@ -36,32 +36,45 @@ class ClearbitPersonService(Service):
     def dispatch(self):
         pass
 
-    def multiprocess(self, poolsize=10):
+    def _merge(self, original_data, output_data):
+        for i in xrange(0, len(original_data)):
+            original_person = original_data[i].values()[0]
+            output_person = output_data[i].values()[0]
+            social_accounts = original_person.get("social_accounts",[]) + output_person.get("social_accounts",[])  
+            linkedin_urls = original_person.get("linkedin_urls") if original_person.get("linkedin_urls") and re.search(pub_profile_re,original_person.get("linkedin_urls")) else output_person.get("linkedin_urls")
+            output_data[i][output_data[i].keys()[0]]["social_accounts"] = social_accounts
+            output_data[i][output_data[i].keys()[0]]["linkedin_urls"] = linkedin_urls
+        return output_data
+
+    def multiprocess(self, poolsize=5, merge=False):
+        #rate limit is 600/minute
         self.logger.info('Starting MultiProcess: %s', 'Clearbit Service')
         pool = multiprocessing.Pool(processes=poolsize)
         self.output = pool.map(unwrap_process_person, self.data)
         pool.close()
         pool.join()
+        if merge:
+            self.output = self._merge(self.data,self.output)
         self.logger.info('Ending MultiProcess: %s', 'Clearbit Service')
         return self.output
 
     def _exclude_person(self, person):
-        if len(person.values()) > 0:
-            if person.values()[0].get("linkedin_urls"):
-                return True
-                #return False
+        if len(person.values()) > 0 and person.values()[0].get("linkedin_urls"):
+            return True
         return False
 
-    def process(self):
+    def process(self, merge=False):
         self.logger.info('Starting Process: %s', 'Clearbit Person Service')
         for person in self.data:
-            if self._exclude_person(person):
+            if self._exclude_person(person) and not merge:
                 self.output.append(person)
             else:
                 email = person.keys()[0]
                 request = ClearbitRequest(email)
                 data = request.get_person()
                 self.output.append(data)
+        if merge:
+            self.output = self._merge(self.data,self.output)                
         self.logger.info('Ending Process: %s', 'Clearbit Person Service')
         return self.output
 
