c7c9c44904bdb4f20444f93908a2de2564f984b6
lauren talbot <laurentracytalbot@gmail.com>
2015-11-23 19:39:29 -0500

    added merge functionality for social accounts and linkedin url

--- prime/processing_service/pipl_service.py
+++ prime/processing_service/pipl_service.py
@@ -108,12 +108,14 @@ class PiplRequest(S3SavedRequest):
         self._build_url()
         super(PiplRequest, self)._make_request()
         pipl_json = None
-        while pipl_json is None:
-            html = self._make_request()
+        tries = 0
+        while pipl_json is None and tries<5:
             try:
+                html = self._make_request()
                 pipl_json = json.loads(html)
             except:
                 pass
+            tries+=1
         social_accounts = self._social_accounts(pipl_json)
         images = self._images(pipl_json)
         linkedin_url = self._linkedin_url(social_accounts)
