8ef38aeb07603ab670429ef5c80dbc2b091451fe
Michael Bishop <michael@advisorconnect.co>
2015-12-07 21:42:19 -0500

    Fix a few typos.

--- BaseDockerImage/build.sh
+++ BaseDockerImage/build.sh
@@ -5,8 +5,8 @@
 
 echo "Make sure you're connected to a local or remote Docker host, this build takes a long time!"
 
-docker build --rm --tag acmichael/python2-onbuild:latest
+docker build --rm --tag acmichael/python2-onbuild:latest .
 
-docker login -u acmichael -p d0gs9900 -e michael@advisorconnect.co
+docker login -u acmichael -e michael@advisorconnect.co
 
 docker push acmichael/python2-onbuild:latest
