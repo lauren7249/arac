2dc7b93ffe6fe658a8f605eb3d1ac7c2e8872ecd
Michael Bishop <michael@advisorconnect.co>
2015-12-13 22:47:56 -0500

    Adding resolvers to machines

--- docker-build-and-run.sh
+++ docker-build-and-run.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
+set -i
+# Think of this as running make <target>
+# The prime image as en 'entrypoint' command of make and a default arg of 'test'
+# run this command with an argument like run, reset, etc as if this shel script
+# where the make command itself.
+
+# Changes in your files should also trigger an auto-rebuild automatically.
+
 
 docker-compose build
 
