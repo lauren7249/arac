6ea32c60118844f77c2b75daaa7ef2fe2f6da546
Michael Bishop <michael@advisorconnect.co>
2015-12-16 17:44:26 -0500

    Refactor worker.py with some retry logic in case Redis is not immediately avaiable.  Only handles retries on RedisCommunication errors, all other errors are propogated unhandled.

--- worker.py
+++ worker.py
@@ -1,13 +1,38 @@
 import os
-import urlparse
-from redis import Redis
+from redis import Redis, ConnectionError
 from rq import Worker, Queue, Connection
+from retrying import retry
 
 listen = ['high', 'default', 'low']
 
-redis_url = os.getenv('REDIS_URL','http://localhost:6379')
-conn = Redis()
+redis_url = os.getenv('REDIS_URL', 'http://localhost:6379')
 
-with Connection(conn):
-    worker = Worker(map(Queue, listen))
-    worker.work()
+
+def retry_if_connection_error(ex):
+    """
+    Return True if we should retry, else return False
+    :param ex: Exception
+    :return: Boolean
+    """
+
+    return isinstance(ex, ConnectionError)
+
+
+@retry(retry_on_exception=retry_if_connection_error, stop_max_attempt_number=10, wait_fixed=5000)
+def do_work():
+    """
+    Activate the Redis worker, retrying up to 10 times at 5 second intervals
+    should redis not be immediately available
+    """
+    try:
+        conn = Redis()
+
+        with Connection(conn):
+            worker = Worker(map(Queue, listen))
+            worker.work()
+    except ConnectionError:
+        pass
+
+
+if __name__ == '__main__':
+    do_work()
