529175d598ffe7a4423d3b67cf20f809f7d2456e
lauren talbot <laurentracytalbot@gmail.com>
2015-12-16 15:20:26 -0500

    edge case

--- prime/processing_service/saved_request.py
+++ prime/processing_service/saved_request.py
@@ -3,7 +3,7 @@ import logging
 import requests
 import boto
 from boto.s3.key import Key
-
+from helper import uu
 from constants import AWS_KEY, AWS_SECRET, AWS_BUCKET, GLOBAL_HEADERS
 S3_BUCKET = boto.connect_s3(AWS_KEY, AWS_SECRET).get_bucket(AWS_BUCKET)
 class S3SavedRequest(object):
@@ -20,7 +20,10 @@ class S3SavedRequest(object):
         self.bucket = S3_BUCKET
 
     def _make_request(self, content_type = 'text/html', bucket=None):
-        self.key = hashlib.md5(self.url).hexdigest()
+        try:
+            self.key = hashlib.md5(self.url).hexdigest()
+        except:
+            self.key = hashlib.md5(uu(self.url)).hexdigest()
         if not bucket:
             bucket = self.bucket
         self.boto_key = Key(bucket)
