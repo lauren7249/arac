693cdb3be001fa25d629f924cd938415620bc6e9
Michael Bishop <michael@advisorconnect.co>
2015-12-17 00:14:08 -0500

    Fix script error preventing new instance from launching

--- DockerScripts/docker-machine-up.sh
+++ DockerScripts/docker-machine-up.sh
@@ -4,7 +4,7 @@ MACHINE=`hostname -s`
 
 docker-machine --debug create  --driver softlayer  --softlayer-api-key 3cee267a0b93e8bd1fb764c3074213602741f0b16642a86f0ced2b76835b0eec \
     --softlayer-cpu "8" --softlayer-disk-size "0" --softlayer-domain "priv.advisorconnect.co" --softlayer-hourly-billing \
-    --softlayer-region "wdc01" --softlayer-user "james@advisorconnect.co" --softlayer-memory "16384"  ${MACHINE}
+    --softlayer-region "dal09" --softlayer-user "james@advisorconnect.co" --softlayer-memory "16384"  ${MACHINE}
 
 eval "$(docker-machine env ${MACHINE})"
 
