349e4149b6b8312c394c29c6b1124d9eebf6629d
lauren talbot <laurentracytalbot@gmail.com>
2016-01-11 18:59:07 -0500

    stupid bing

--- prime/processing_service/bing_request.py
+++ prime/processing_service/bing_request.py
@@ -164,15 +164,27 @@ class BingRequest(S3SavedRequest):
         if key.exists():
             self.logger.info('Make Request: %s', 'Get From S3')
             html = key.get_contents_as_string()
-        else:
+            #only return if we didnt save a stupid error response from stupid bing like we were doing in the last stupid version of this function.
             try:
-                response = requests.get(self.next_querystring + "&$format=json" , auth=(api_key, api_key))
-                html = response.content
+                raw_results = json.loads(html.decode("utf-8-sig"))['d']
+                return html
             except:
-                html = None
-            if html:
+                pass
+        #guess that key didnt exist or the results were stupid
+        try:
+            response = requests.get(self.next_querystring + "&$format=json" , auth=(api_key, api_key))
+            html = response.content
+        except:
+            html = None
+        #at this point we have tried everything so better just leave it alone, return whatever crap we got and hope for the best.
+        if html:
+            try:
+                #only save if it's not an error response from stupid bing.
+                raw_results = json.loads(html.decode("utf-8-sig"))['d']            
                 key.content_type = 'text/html'
                 key.set_contents_from_string(html)
+            except:
+                pass
         return html
 
     def process(self):
