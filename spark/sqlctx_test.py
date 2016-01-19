f37f63e0c548e55b0ddccd218fa1a1c63b11033b
lauren talbot <laurentracytalbot@gmail.com>
2016-01-19 01:57:50 -0500

    moving things around to make things more conducive to doing other kinds of work

--- spark/sqlctx_test.py
+++ spark/sqlctx_test.py
@@ -5,7 +5,7 @@ import happybase
 
 class PeopleFetcher(object):
 
-    def __init__(self, period, sc, sqlCtx, obs=None):
+    def __init__(self, period, sc, sqlCtx, obs=None, file_pattern='.jl'):
         self.sc = sc
         self.sqlCtx = sqlCtx
         self.AWS_KEY = "AKIAIZZBJ527CKPNY6YQ"
@@ -14,7 +14,7 @@ class PeopleFetcher(object):
         self.sc._jsc.hadoopConfiguration().set("fs.s3n.awsSecretAccessKey", self.AWS_SECRET)
         self.AWS_BUCKET = "ac-crawlera"
         self.PERIOD = period
-        self.people_rdd = self.sqlCtx.read.json("s3n://" + self.AWS_BUCKET + "/linkedin/people/" + self.PERIOD + "/*94.jl")
+        self.people_rdd = self.sqlCtx.read.json("s3n://" + self.AWS_BUCKET + "/linkedin/people/" + self.PERIOD + "/*" + file_pattern)
         self.people_rdd.registerTempTable("people")
         # self.sqlCtx.registerFunction("get_dob_year_range",get_dob_year_range)
         # self.people_dataFrame = self.sqlCtx.sql("""select *, get_dob_year_range(education, experience) as dob_year_range, dob_year_range[0] as dob_year_min, dob_year_range[1] as dob_year_max from people """).drop("dob_year_range")
