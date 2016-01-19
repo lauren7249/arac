49a30cc27b32795cf55d9ceac3dbe095ef3c3131
lauren talbot <laurentracytalbot@gmail.com>
2016-01-19 01:56:24 -0500

    moving things around to make things more conducive to doing other kinds of work

--- spark/sqlctx_test.py
+++ spark/sqlctx_test.py
@@ -16,11 +16,11 @@ class PeopleFetcher(object):
         self.PERIOD = period
         self.people_rdd = self.sqlCtx.read.json("s3n://" + self.AWS_BUCKET + "/linkedin/people/" + self.PERIOD + "/*94.jl")
         self.people_rdd.registerTempTable("people")
-        self.sqlCtx.registerFunction("get_dob_year_range",get_dob_year_range)
-        self.people_dataFrame = self.sqlCtx.sql("""select *, get_dob_year_range(education, experience) as dob_year_range, dob_year_range[0] as dob_year_min, dob_year_range[1] as dob_year_max from people """).drop("dob_year_range")
-        if obs:
-            self.people_dataFrame = self.people_dataFrame.limit(obs)
-        self.people_dataFrame.cache()
+        # self.sqlCtx.registerFunction("get_dob_year_range",get_dob_year_range)
+        # self.people_dataFrame = self.sqlCtx.sql("""select *, get_dob_year_range(education, experience) as dob_year_range, dob_year_range[0] as dob_year_min, dob_year_range[1] as dob_year_max from people """).drop("dob_year_range")
+        # if obs:
+        #     self.people_dataFrame = self.people_dataFrame.limit(obs)
+        # self.people_dataFrame.cache()
 
         def get_people_viewed_also(self, url):
             also_viewed = self.people_dataFrame.where(array_contains(self.people_dataFrame.also_viewed,url)==True)
