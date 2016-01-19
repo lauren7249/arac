146546fcd35e8d8cc984cfb2fee9aa9c299ffad6
lauren talbot <laurentracytalbot@gmail.com>
2016-01-19 00:50:34 -0500

    moving things around to make things more conducive to doing other kinds of work

--- spark/sqlctx_test.py
+++ spark/sqlctx_test.py
@@ -1,11 +1,11 @@
 from pyspark.sql.functions import array_contains
 from helpers.linkedin_helpers import get_dob_year_range
 from prime.utils.crawlera import reformat_crawlera
-#test function
+import happybase
 
 class PeopleFetcher(object):
 
-    def __init__(self, period, sc, sqlCtx):
+    def __init__(self, period, sc, sqlCtx, obs=None):
         self.sc = sc
         self.AWS_KEY = "AKIAIZZBJ527CKPNY6YQ"
         self.AWS_SECRET = "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"
@@ -18,8 +18,15 @@ class PeopleFetcher(object):
         self.people_rdd = self.sc.jsonFile(self.filenames)
         self.people_rdd.registerTempTable("people")
         self.sqlCtx.registerFunction("get_dob_year_range",get_dob_year_range)
-        self.people_dataFrame = self.sqlCtx.sql("""select *, get_dob_year_range(education, experience) as dob_year_range, dob_year_range[0] as dob_year_min, dob_year_range[1] as dob_year_max from people """)
+        self.people_dataFrame = self.sqlCtx.sql("""select *, get_dob_year_range(education, experience) as dob_year_range, dob_year_range[0] as dob_year_min, dob_year_range[1] as dob_year_max from people """).drop("dob_year_range")
+        if obs:
+            self.people_dataFrame = self.people_dataFrame.limit(obs)
+        self.people_dataFrame.cache()
 
-        def get_viewed_also(self, url):
+        def get_people_viewed_also(self, url):
             also_viewed = self.people_dataFrame.where(array_contains(self.people_dataFrame.also_viewed,url)==True)
             return also_viewed.collect()
+
+        #def get_person(self, linkedin_id=None, url=None):
+
+        # def query(self, name, dob_year):
