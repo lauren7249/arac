c7f8347610707b4b663519441d275b10d871cef1
lauren talbot <laurentracytalbot@gmail.com>
2016-01-19 01:45:03 -0500

    moving things around to make things more conducive to doing other kinds of work

--- spark/sqlctx_test.py
+++ spark/sqlctx_test.py
@@ -2,8 +2,6 @@ from pyspark.sql.functions import array_contains
 from helpers.linkedin_helpers import get_dob_year_range
 from prime.utils.crawlera import reformat_crawlera
 import happybase
-import boto
-from boto.s3.key import Key
 
 class PeopleFetcher(object):
 
@@ -12,13 +10,11 @@ class PeopleFetcher(object):
         self.sqlCtx = sqlCtx
         self.AWS_KEY = "AKIAIZZBJ527CKPNY6YQ"
         self.AWS_SECRET = "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"
+        self.sc._jsc.hadoopConfiguration().set("fs.s3n.awsAccessKeyId",self.AWS_KEY)
+        self.sc._jsc.hadoopConfiguration().set("fs.s3n.awsSecretAccessKey", self.AWS_SECRET)
         self.AWS_BUCKET = "ac-crawlera"
-        self.S3_BUCKET = boto.connect_s3(self.AWS_KEY, self.AWS_SECRET).get_bucket(self.AWS_BUCKET)
         self.PERIOD = period
-        self.keys = self.S3_BUCKET.list("linkedin/people/" + self.PERIOD + "/")
-        self.keypaths = ["s3a://" + self.AWS_KEY + ":" + self.AWS_SECRET + "@" + self.AWS_BUCKET + "/" + key.name for key in self.keys]
-        self.filenames = ",".join(self.keypaths)
-        self.people_rdd = self.sqlCtx.jsonFile(self.filenames)
+        self.people_rdd = self.sqlCtx.read.json("s3n://" + self.AWS_BUCKET + "/linkedin/people/" + self.PERIOD + "/*94.jl")
         self.people_rdd.registerTempTable("people")
         self.sqlCtx.registerFunction("get_dob_year_range",get_dob_year_range)
         self.people_dataFrame = self.sqlCtx.sql("""select *, get_dob_year_range(education, experience) as dob_year_range, dob_year_range[0] as dob_year_min, dob_year_range[1] as dob_year_max from people """).drop("dob_year_range")
