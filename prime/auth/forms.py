f57de8f13ec2314675edb7ccbe0e612c591f169a
lauren talbot <laurentracytalbot@gmail.com>
2016-01-20 10:21:04 -0500

    bug fix

--- prime/auth/forms.py
+++ prime/auth/forms.py
@@ -1,9 +1,10 @@
 from flask.ext.wtf import Form
 from wtforms import StringField, PasswordField, HiddenField
 from wtforms.validators import Length, Email, InputRequired, EqualTo
+from prime.users.models import User
 
 class LoginForm(Form):
-    from prime.users.models import User
+
     email = StringField('Email', validators=[InputRequired(), Email()])
     password = PasswordField('Password', validators=[InputRequired()])
 
