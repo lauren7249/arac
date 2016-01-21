5187e6438516994277e6f0836f665f08573992ba
lauren talbot <laurentracytalbot@gmail.com>
2016-01-21 02:06:22 -0500

    using zip files

--- spark/hbase_load_simple.py
+++ spark/hbase_load_simple.py
@@ -1,6 +1,8 @@
 import boto, re
 from boto.s3.key import Key
 import happybase
+import gzip
+import StringIO
 from flatmappers import *
 from foldbykey import *
 
@@ -11,12 +13,10 @@ class HBaseLoader(object):
         self.AWS_KEY = "AKIAIZZBJ527CKPNY6YQ"
         self.AWS_SECRET = "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"
         self.AWS_BUCKET = "ac-crawlera"
-        self.S3_BUCKET = boto.connect_s3(self.AWS_KEY, self.AWS_SECRET).get_bucket(self.AWS_BUCKET)
+        self.AWS_BUCKET_ZIPPED = "crawlera-linkedin-profiles"
+        self.S3_BUCKET = boto.connect_s3(self.AWS_KEY, self.AWS_SECRET).get_bucket(self.AWS_BUCKET_ZIPPED)
+        self.S3_BUCKET_ZIPPED = boto.connect_s3(self.AWS_KEY, self.AWS_SECRET).get_bucket(self.AWS_BUCKET_ZIPPED)
         self.PERIOD = period
-        self.keys = self.S3_BUCKET.list("linkedin/people/" + self.PERIOD + "/")
-        self.keypaths = ["s3a://" + self.AWS_KEY + ":" + self.AWS_SECRET + "@" + self.AWS_BUCKET + "/" + key.name for key in self.keys]
-        self.filenames = ",".join(self.keypaths)
-        self.data = self.sc.textFile(self.filenames)
         self.keyConv_read = "org.apache.spark.examples.pythonconverters.ImmutableBytesWritableToStringConverter"
         self.valueConv_read = "org.apache.spark.examples.pythonconverters.HBaseResultToStringConverter"                     
         self.keyConv_write = "org.apache.spark.examples.pythonconverters.StringToImmutableBytesWritableConverter"
@@ -29,6 +29,13 @@ class HBaseLoader(object):
         self.conf = {"mapreduce.outputformat.class": self.table_output_format,  
                     "mapreduce.job.output.key.class": self.key_class,  
                      "mapreduce.job.output.value.class": self.value_class}
+
+    def get_s3_data(self):
+        self.keys = self.S3_BUCKET.list("linkedin/people/" + self.PERIOD + "/")
+        self.keypaths = ["s3a://" + self.AWS_KEY + ":" + self.AWS_SECRET + "@" + self.AWS_BUCKET_ZIPPED + "/" + key.name for key in self.keys]
+        self.filenames = ",".join(self.keypaths)
+        self.data = self.sc.textFile(self.filenames)
+        return self.data
 
     #30 minutes?
     def load_people_table(self):          
