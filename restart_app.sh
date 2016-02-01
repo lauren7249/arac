b0ea644be9f875a2a455a4b7623d05951378e2de
lauren talbot <laurentracytalbot@gmail.com>
2016-02-01 00:47:11 -0500

    faces

--- restart_app.sh
+++ restart_app.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-export AC_CONFIG='beta'
+# export AC_CONFIG='beta'
 . ~/env/bin/activate
 pip install -r requirements.txt 
-sudo fuser -k 80/tcp
-sudo rm true
+sudo pkill -f track_errors.py
+# sudo fuser -k 80/tcp
+# sudo rm true
 sudo rm nohup.out
 sudo pkill -f worker.py
+
 nohup python worker.py &
-sudo ~/env/bin/uwsgi production.ini
-sudo nohup ~/env/bin/python track_errors.py &>/dev/null
+# sudo ~/env/bin/uwsgi production.ini
+sudo nohup python track_errors.py &>/dev/null
 
