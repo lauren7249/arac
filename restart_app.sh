2504dd55f1a6eb253c23eaed7c2ca269d80cd025
lauren talbot <laurentracytalbot@gmail.com>
2016-02-01 11:59:59 -0500

    yikes

--- restart_app.sh
+++ restart_app.sh
@@ -2,15 +2,15 @@
 export AC_CONFIG='beta'
 . ~/env/bin/activate
 pip install -r requirements.txt 
-sudo pkill -f track_errors.py
-sudo fuser -k 80/tcp
-sudo rm true
-sudo rm nohup.out
-sudo pkill -f worker.py
-sudo nohup python worker.py &
+pkill -f track_errors.py
+fuser -k 80/tcp
+rm true
+rm nohup.out
+pkill -f worker.py
+nohup python worker.py &
 sleep 1
-sudo ~/env/bin/uwsgi production.ini
+~/env/bin/uwsgi production.ini
 sleep 2
-sudo nohup python track_errors.py & tail -f nohup.out 
+nohup python track_errors.py & tail -f nohup.out 
 
 
