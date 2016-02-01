507a30a50c4f284d62aa194ad11ae1b86cd94b69
lauren talbot <laurentracytalbot@gmail.com>
2016-01-31 22:59:31 -0500

    restart changed

--- restart_app.sh
+++ restart_app.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 export AC_CONFIG='beta'
 . ~/env/bin/activate
+pip install -r requirements.txt 
 sudo fuser -k 80/tcp
-sleep 1
+sudo rm true
+sudo rm nohup.out
+sudo pkill -f worker.py
+nohup python worker.py &
 sudo ~/env/bin/uwsgi production.ini
+sudo nohup python track_errors.py &>/dev/null
+
