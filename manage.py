b18bf8311b6a9dac7c65f7023459bb0686841f36
lauren talbot <laurentracytalbot@gmail.com>
2016-02-01 11:18:49 -0500

    for ana

--- manage.py
+++ manage.py
@@ -43,68 +43,22 @@ if __name__ == '__main__':
         session.commit()
 
     @manager.command
-    def rerun_contacts(email, hired):
+    def rerun_contacts(email, hired, flush):
         from prime.users.models import db, User
         from prime.prospects.views import queue_processing_service
-        from prime.processing_service.saved_request import UserRequest
-        from redis import Redis
-        from rq import Queue
         session = db.session
         user = User.query.filter_by(email=email).first()
-        user_request = UserRequest(email)
-        contacts_array = user_request.lookup_data()
-        from_linkedin = set()
-        from_gmail = set()
-        from_yahoo = set()
-        from_windowslive = set()
-        from_aol = set()
-        from_all = set()
-        account_sources = {}
-        for record in contacts_array:
-            owner = record.get("contacts_owner")              
-            if owner:
-                account_email = owner.get("email",[{}])[0].get("address","").lower()   
-            else: 
-                account_email = 'linkedin'                    
-            service = record.get("service","").lower()
-            account_sources[account_email] = service
-            contact = record.get("contact",{})
-            emails = contact.get("email",[{}])
-            try:
-                email_address = emails[0].get("address",'').lower()
-            except Exception, e:
-                email_address = ''
-                print emails
-            if not email_address:
-                continue   
-            from_all.add(email_address)  
-            if service=='linkedin':
-                from_linkedin.add(email_address)
-            elif service=='gmail':
-                from_gmail.add(email_address)
-            elif service=='yahoo':
-                from_yahoo.add(email_address)
-            elif service=='windowslive':
-                from_windowslive.add(email_address)
-            elif service=='aol':
-                from_aol.add(email_address)       
-        user.unique_contacts_uploaded = len(from_all)
-        user.contacts_from_linkedin = len(from_linkedin)
-        user.contacts_from_gmail = len(from_gmail)
-        user.contacts_from_yahoo = len(from_yahoo)
-        user.contacts_from_windowslive = len(from_windowslive)
-        user.contacts_from_aol = len(from_aol)
-        user.account_sources = account_sources
-        user._statistics = None
-        for client_prospect in user.client_prospects:
-            session.delete(client_prospect)
-        session.add(user)
-        session.commit()        
+        contacts_array, user = user.refresh_contacts()
+        print len(contacts_array)
         client_data = {"first_name":user.first_name,"last_name":user.last_name,\
                 "email":user.email,"location":user.location,"url":user.linkedin_url,\
                 "to_email":user.manager.user.email, "hired": (hired == "True")}
         q = get_q()
-        q.enqueue(queue_processing_service, client_data, contacts_array, timeout=14400)
+        q.enqueue(queue_processing_service, client_data, contacts_array, timeout=14400)        
+        if flush=='True':
+            for client_prospect in user.client_prospects:
+                session.delete(client_prospect)
+            session.commit()        
 
     @manager.command
     def delete_user(email):
@@ -127,6 +81,26 @@ if __name__ == '__main__':
             if re.search(pattern, email):
                 print email
                 delete_user(email)
+
+    @manager.command
+    def add_facebook(email, filename):
+        import pandas
+        from prime.users.models import User
+        from prime.prospects.views import queue_processing_service
+        user = User.query.filter_by(email=email).first()
+        s = pandas.read_csv(filename, header=None)
+        s.fillna("", inplace=True)
+        data = []
+        for index, row in s.iterrows():
+            contact = {}
+            contact["email"] = [{"address": row[0].strip() + "@facebook.com"}]
+            data.append(contact)      
+        client_data = {"first_name":user.first_name,"last_name":user.last_name,\
+                "email":user.email,"location":user.location,"url":user.linkedin_url, "hired": True}
+        q = get_q()
+        q.enqueue(queue_processing_service, client_data, data, timeout=14400)            
+
+
 
     manager.add_command('db', MigrateCommand)
     manager.add_command('shell', Shell(use_ipython=True))
