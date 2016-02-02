ed5450f0391fbe3ab41ce6d23409f9b1a9502aff
Server <james@advisorconnect.co>
2016-02-01 23:27:42 -0600

    try to add laurens script

--- restart_app.sh
+++ restart_app.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 export AC_CONFIG='beta'
-sudo . ~/env/bin/activate
-pip install -r requirements.txt 
+. ./vars.sh
+. ~/env/bin/activate
+#pip install -r requirements.txt 
 sudo pkill -f track_errors.py
 sudo fuser -k 80/tcp
-sudo rm true
+sudo rm -f true 
 sudo rm nohup.out
 sleep 1
-~/env/bin/uwsgi production.ini
+sudo ~/env/bin/uwsgi production.ini
 sudo pkill -f worker.py
 nohup python worker.py & 
-sleep 2
-nohup python track_errors.py & 
+sleep 5
+sudo nohup python track_errors.py & 
 
 
