41ecd99fc53b5f97e65dfe080fa98cffd57a71b5
Michael Bishop <michael@advisorconnect.co>
2016-02-10 01:09:10 -0500

    Continuing rewrite of makefile

--- manage.sh
+++ manage.sh
@@ -30,26 +30,35 @@ declare -r -i E_MISSING_PARAM=75
 declare -r -i E_OPTERROR=85
 declare -r -i NO_ARGS=0
 
+# @formatter:off
+# Ensure parameter was passed
+if [ $# -eq "$NO_ARGS" ] # Script invoked with no command-line args?
+then
+    echo "Usage: $(basename $0) options (prod_run dev_run test_run )"
+    exit "${E_OPTERROR}";
+fi
+# @formatter:on
 
 # Run in development mode
-function dev_run {
+dev_run() {
     wait_until_is_ready
     first_setup_check
     run_worker
     run_uwsgi
+    return $?;
 }
 
-function prod_run {
-    return 0;
+prod_run() {
+    :
 }
 
 # Launch worker and background
-function run_worker {
+run_worker() {
     $(${PYTHON} ./worker.py &)
     return 0;
 }
 
-function run_uwsgi {
+run_uwsgi() {
     $(${UWSGI} --ini production.ini)
     return $?;
 }
@@ -57,7 +66,7 @@ function run_uwsgi {
 # Check if the database exists and creates if not
 # found.
 # TODO: This should be a more rigorous check
-function first_setup_check {
+first_setup_check() {
 
     # Return code is 0 only if database exists
     local EXISTS=$(${PG_LIST} | grep "${PG_DB}" -q)
@@ -66,46 +75,42 @@ function first_setup_check {
     if [ "${EXISTS}" -neq 0 ]; then
         create_db
     fi
+    return 0;
 }
 
-function reset_db {
-    exit 0;
+reset_db() {
+    :
 }
 
 # Drop the database
-function drop_db {
-    $(${PG_BINDIR}/dropdb -h "${PG_HOST}" -U "${PG_USER}" -w --if-exists "${PG_DB}");
+drop_db() {
+    $(${PG_BINDIR}/dropdb -h "${PG_HOST}" -U "${PG_USER}" -w --if-exists "${PG_DB}")
+    return 0;
 }
 
 # Create the default app user and database
 # TODO: Handle failures
-function create_db {
-    { $(${PG_BINDIR}/createuser -h "${PG_HOST}" -U "${PG_USER}" -d -s -w "${PG_DB}") } || true
-    { $(${PG_BINDIR}/createdb -h "${PG_HOST}" -l "${PG_LOCALE}" -w -U "${PG_USER}" "${PG_DB}") } || true
+create_db() {
+    { $(${PG_BINDIR}/createuser -h "${PG_HOST}" -U "${PG_USER}" -d -s -w "${PG_DB}"); } || true
+    { $(${PG_BINDIR}/createdb -h "${PG_HOST}" -l "${PG_LOCALE}" -w -U "${PG_USER}" "${PG_DB}"); } || true
     $(${PYTHON} ./manage.py db upgrade)
+    return 0;
 }
 
 # Block until the database is up and answering
-function wait_until_is_ready {
+wait_until_is_ready() {
     until $(${PG_BINDIR}/pg_isready -h "${PG_HOST}" -U "${PG_USER}"); do
         sleep 5;
-    done;
+    done
+    return $?;
 }
 
 # Run test scripts
-function test_run {
+test_run() {
     $(${PYTHON} ./manage.py test)
+    return $?;
 }
 
 # Entrypoint into script
-
-# Ensure parameter was passed
-if [ $# -eq "$NO_ARGS" ] # Script invoked with no command-line args?
-then
-    echo "Usage: `basename$0` options (prod_run dev_run test_run )"
-    exit "${E_OPTERROR}"
-
-
-fi
 
 exit $?
