30ac559a7c7d916bfadc96ad0d3d07fbb282915b
Michael Bishop <michael@advisorconnect.co>
2016-02-10 00:51:01 -0500

    Continuing rewrite of makefile

--- manage.sh
+++ manage.sh
@@ -9,18 +9,27 @@
 #
 
 # Executables
-declare PG_BINDIR="`pg_config--bindir`"
-declare PYTHON="`whichpython`"
-declare UWSGI="`whichuwsgi`"
+# @formatter:off
+declare -r PG_BINDIR=$(pg_config --bindir)
+declare -r PYTHON=$(which python)
+declare -r UWSGI=$(which uwsgi)
+# @formatter:on
 
-declare PG_LOCALE="${LC_ALL:-'en_US.utf8'}"
-declare PG_USER="${DB_USER:-'postgres'}"
-declare PG_PASS="${DB_PASS}"
-declare PG_DB="${PG_DB:-'arachnid'}"
-declare PG_HOST="${PG_HOST}"
+# Postgres settings
+declare -x PG_LOCALE="${LC_ALL:-'en_US.utf8'}"
+declare -x PG_USER="${DB_USER:-'postgres'}"
+declare -x PG_PASS="${DB_PASS}"
+declare -x PG_DB="${PG_DB:-'arachnid'}"
+declare -x PG_HOST="${PG_HOST}"
 
 # List of databases command
-declare PG_LIST="${PG_BINDIR}/psql -h ${PG_HOST} -U ${PG_USER} -l"
+declare -r PG_LIST="${PG_BINDIR}/psql -h ${PG_HOST} -U ${PG_USER} -l"
+
+# Error messages and conditions
+declare -r -i E_MISSING_PARAM=75
+declare -r -i E_OPTERROR=85
+declare -r -i NO_ARGS=0
+
 
 # Run in development mode
 function dev_run {
@@ -36,13 +45,12 @@ function prod_run {
 
 # Launch worker and background
 function run_worker {
-
-    `${PYTHON} ./worker.py &`
+    $(${PYTHON} ./worker.py &)
     return 0;
 }
 
 function run_uwsgi {
-    `${UWSGI} --ini production.ini`
+    $(${UWSGI} --ini production.ini)
     return $?;
 }
 
@@ -52,7 +60,7 @@ function run_uwsgi {
 function first_setup_check {
 
     # Return code is 0 only if database exists
-    local EXISTS = `${PG_LIST} | grep "${PG_DB}" -q`
+    local EXISTS=$(${PG_LIST} | grep "${PG_DB}" -q)
 
     # For any other return code, create the database
     if [ "${EXISTS}" -neq 0 ]; then
@@ -66,28 +74,38 @@ function reset_db {
 
 # Drop the database
 function drop_db {
-    `${PG_BINDIR}/dropdb -h "${PG_HOST}" -U "${PG_USER}" -w --if-exists "${PG_DB}"`;
+    $(${PG_BINDIR}/dropdb -h "${PG_HOST}" -U "${PG_USER}" -w --if-exists "${PG_DB}");
 }
 
 # Create the default app user and database
 # TODO: Handle failures
 function create_db {
-    { `${PG_BINDIR}/createuser -h "${PG_HOST}" -U "${PG_USER}" -d -s -w "${PG_DB}"`
-        `${PG_BINDIR}/createdb -h "${PG_HOST}" -l "${PG_LOCALE}" -w -U "${PG_USER}" "${PG_DB}"`
-        `${PYTHON} ./manage.py db upgrade` } || true
+    { $(${PG_BINDIR}/createuser -h "${PG_HOST}" -U "${PG_USER}" -d -s -w "${PG_DB}") } || true
+    { $(${PG_BINDIR}/createdb -h "${PG_HOST}" -l "${PG_LOCALE}" -w -U "${PG_USER}" "${PG_DB}") } || true
+    $(${PYTHON} ./manage.py db upgrade)
 }
 
 # Block until the database is up and answering
 function wait_until_is_ready {
-    until `${PG_BINDIR}/pg_isready -h "${PG_HOST}" -U "${PG_USER}"`; do
+    until $(${PG_BINDIR}/pg_isready -h "${PG_HOST}" -U "${PG_USER}"); do
         sleep 5;
     done;
 }
 
 # Run test scripts
-function test_app {
-    {
-        `${PYTHON} ./manage.py test`
-    };
+function test_run {
+    $(${PYTHON} ./manage.py test)
 }
 
+# Entrypoint into script
+
+# Ensure parameter was passed
+if [ $# -eq "$NO_ARGS" ] # Script invoked with no command-line args?
+then
+    echo "Usage: `basename$0` options (prod_run dev_run test_run )"
+    exit "${E_OPTERROR}"
+
+
+fi
+
+exit $?
