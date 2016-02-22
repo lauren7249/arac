5d2626215da68abed3d6cbda9512b9be3477eb1f
James Johnson <jamesjohnson11@gmail.com>
2016-02-22 17:28:01 -0500

    got pin flow working

--- prime/prospects/views.py
+++ prime/prospects/views.py
@@ -138,6 +138,7 @@ def start_linkedin_login_bot(email, password, user_id):
     # pin Second is linkedin asked for a pin, which they sent via email
     # credentials Uername is wrong, or password is wrong
     # unknown TODO unknown negative outcome
+    conn = get_conn()
     error, pin_requested = getter.check_linkedin_login_errors()
     if error is None:
         #Everything worked
@@ -145,15 +146,14 @@ def start_linkedin_login_bot(email, password, user_id):
         current_user.set_linkedin_password(password, session=session)
         session.add(current_user)
         session.commit()
-        conn = get_conn()
         data = None
-        #tries = 0
-        #while(data == None and tries<4):
-        #    data = getter.get_linkedin_data()
-        #    tries += 1
-        #getter.quit()
-        #if data:
-        #    contacts_array, user = current_user.refresh_contacts(new_contacts=data, service_filter='linkedin', session=session)
+        tries = 0
+        while(data == None and tries<4):
+            data = getter.get_linkedin_data()
+            tries += 1
+        getter.quit()
+        if data:
+            contacts_array, user = current_user.refresh_contacts(new_contacts=data, service_filter='linkedin', session=session)
         conn.hset("linkedin_login_outcome", email, "success:True")
         return True
 
@@ -163,11 +163,12 @@ def start_linkedin_login_bot(email, password, user_id):
         current_user.set_linkedin_password(password, session=session)
         session.add(current_user)
         session.commit()
+        conn.hset("linkedin_login_outcome", email, "pin:{}".format(error))
         q = get_q()
         q.enqueue(selenium_state_holder, getter, current_user.user_id, timeout=140400)
-        conn.hset("linkedin_login_outcome", email, "pin_requested:{}".format(error))
         return True
     #username or password was wrong
+    conn = get_conn()
     conn.hset("linkedin_login_outcome", email, "error:{}".format(error))
     return True
 
@@ -257,11 +258,12 @@ def linkedin_login():
 @prospects.route('/linkedin_pin', methods=['GET', 'POST'])
 def linkedin_pin():
     pin_worked = None
-    pin = request.args.get("pin")
-    message = request.args.get("message")
-    email = current_user.linkedin_login_email
-    if pin and email:
-        pin_worked = give_pin(email, pin)
+    if request.method == 'POST':
+        pin = request.form.get("pin")
+        email = current_user.linkedin_login_email
+        if pin and email:
+            pin_worked = give_pin(email, pin)
+        return jsonify({"success": True, "finished": True})
     return render_template('linkedin_pin.html',pin_worked=pin_worked)
 
 @csrf.exempt
