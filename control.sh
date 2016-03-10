a7afab2f7fc2e03fa6562f85c921ad54e268c2f7
lauren talbot <laurentracytalbot@gmail.com>
2016-03-10 12:16:26 -0500

    trying to fix

--- control.sh
+++ control.sh
@@ -128,7 +128,7 @@ drop_db() {
 create_dev_db() {
     local CREATE_USR_CMD="${PG_BINDIR}/createuser -h ${PG_HOST} -U ${PG_USER} -d -s -w ${PG_DB}"
     local CREATE_DB_CMD="${PG_BINDIR}/createdb -h ${PG_HOST} -l ${PG_LOCALE} -w -U ${PG_USER} ${PG_DB}"
-    local LOAD_DB_CMD="${PG_BINDIR}/psql ${PG_DB} -h ${PG_HOST} -w -U ${PG_USER} -f ${PG_DUMP_FILE}"
+    local LOAD_DB_CMD="${PG_BINDIR}/psql ${PG_DB} -h ${PG_HOST} -w -l ${PG_LOCALE} -U ${PG_USER} -f ${PG_DUMP_FILE}" 
     local MIGRATION_INIT_DB_CMD="./manage.py db init"
     local MIGRATION_DB_CMD="./manage.py db migrate"
     local UPGRADE_DB_CMD="./manage.py db upgrade"
