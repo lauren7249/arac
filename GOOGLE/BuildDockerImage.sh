08034e45756cba881ce1360c317d449fc6596892
Michael Bishop <michael@advisorconnect.co>
2016-04-21 10:17:32 -0400

    Refactor build scripts

--- GOOGLE/BuildDockerImage.sh
+++ GOOGLE/BuildDockerImage.sh
@@ -2,7 +2,7 @@
 
 REGISTRY="gcr.io/advisorconnect-1238/"
 IMAGE="aconn"
-VERSION="1.0.6"
+VERSION="1.0.8"
 
 BRANCH=`git branch | head -n 1 | cut -c 3-`
 COMMIT=`git log  | head -n1 | cut -c 8-16`
@@ -14,7 +14,7 @@ echo "COMMIT: $COMMIT"
 sed -l -e "s/BRANCH--/${BRANCH}/g" -e "s/COMMIT--/${COMMIT}/g" Dockerfile > Dockerfile.tmp
 
 gcloud docker push -a
-docker --tls build --pull --rm -t ${REGISTRY}${IMAGE}:${VERSION} -f Dockerfile.tmp .
+docker --tls build --pull --rm -t ${REGISTRY}${IMAGE}:${VERSION} -f Dockerfile.tmp ..
 docker --tls push ${REGISTRY}${IMAGE}:${VERSION}
 docker --tls tag  ${REGISTRY}${IMAGE}:${VERSION} ${REGISTRY}${IMAGE}:latest
 docker --tls push ${REGISTRY}${IMAGE}:${VERSION}
